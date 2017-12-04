97e45f383224f5c1d9e75cdaccc1bef178c9656c
yuanq <yq911122@gmail.com>
2017-12-04 12:00:17 -0800

    Create data reader

--- rnn/reader.py
+++ rnn/reader.py
@@ -24,30 +24,39 @@ import os
 import sys
 
 import tensorflow as tf
-from gensim.models import Word2Vec
+import gensim
+import numpy as np
 
-model = Word2Vec.load_word2vec_format('../../models/GoogleNews-vectors-negative300.bin.gz', binary=True)
+model = gensim.models.KeyedVectors.load_word2vec_format('../models/GoogleNews-vectors-negative300.bin.gz', binary=True)
 word2vec_dim = 300
 
 ner_cls = {}
+maxlen = 0
 
-def load_ner_cls(ners):
+def load_ner_cls(ner_lists):
+    ners = [ner for ners in ner_lists for ner in ners]
     for i, ner in enumerate(set(ners)):
         ner_cls[ner] = i 
+        
+def get_maxlen(utt_lists):
+    return max([len(utt) for utt in utt_lists])
 
-def load(data_path):
-    train_path = os.path.join(data_path, "astro.train.txt")
-    valid_path = os.path.join(data_path, "astro.valid.txt")
-    test_path = os.path.join(data_path, "astro.test.txt")
+def load_from_path(data_path):
+    train_path = os.path.join(data_path, "astro_tmp.train.tsv")
+    valid_path = os.path.join(data_path, "astro_tmp.valid.tsv")
+    test_path = os.path.join(data_path, "astro_tmp.test.tsv")
 
-    train_data = AstroData(_read_data(train_path))
-    valid_data = AstroData(_read_data(valid_path))
-    test_data = AstroData(_read_data(test_path))
+    train_data = AstroData(_read_data(train_path), model, word2vec_dim)
+    valid_data = AstroData(_read_data(valid_path), model, word2vec_dim)
+    test_data = AstroData(_read_data(test_path), model, word2vec_dim)
     
-    load_ner_cls(train_data.ner)
+    load_ner_cls(train_data.ners + valid_data.ners + test_data.ners)
+    global maxlen
+    maxlen = get_maxlen(train_data.utterances + valid_data.utterances + test_data.utterances)
     
     for data in [train_data, valid_data, test_data]:
         data.numerize_ner_cls()
+        data.pad_seqlen(maxlen)
 
     return train_data, valid_data, test_data
 
@@ -73,7 +82,7 @@ def _read_data(filename):
     with tf.gfile.GFile(filename, "r") as f:
         data = f.read().decode("utf-8").split('\n\n')
         data = [l.split('\n') for l in data]
-        data = [[w.split('\t') for w in l] for l in data]
+        data = [[w.split('\t') for w in l] for l in data if len(l) > 0]
         return data
 
 class AstroData(object):
@@ -81,15 +90,19 @@ class AstroData(object):
     """input is output of _read_data"""
 
     def __init__(self, data, model, word2vec_dim):
+        self.utterances = []
+        self.ners = []
+        self.seqlen = []
         for utt in data:
             words, ners = [], []
             for word, ner in utt:
                 word_vec = AstroData._word2vec(word, model, word2vec_dim)
-                if not word_vec: continue
+#                 if not word_vec: continue
                 words.append(word_vec)
                 ners.append(AstroData.get_ner(ner, iob=False))
-            self.utterance.append(words)
+            self.utterances.append(words)
             self.ners.append(ners)
+            self.seqlen.append(len(words))
                     
 #         self.classification
 #         self.intent
@@ -100,7 +113,7 @@ class AstroData(object):
         else: return ner if ner == 'O' else ner[2:]
     
     def numerize_ner_cls(self):
-        self.ner = [AstroData._ner_to_id(n) for n in self.ner]        
+        self.ners = [[self._ner_to_id(n) for n in ner] for ner in self.ners]        
     
 #     def word2vec(self, model, word2vec_dim):
 #         self.utterance = [[AstroData._word2vec(w, model) for w in utt] for utt in self.utterance]
@@ -110,78 +123,62 @@ class AstroData(object):
         if word in model.wv.vocab:
             return model.wv[word]
         else:
-            print("word %s not found in model." % (word))
-            return [0.] * word2vec_dim
+#             print("word %s not found in model." % (word))
+            return np.zeros(word2vec_dim, dtype=np.float32)
 
-    @classmethod
-    def _ner_to_id(cls, ner):
+    def _ner_to_id(self, ner):
         try:
-            return ner_cls[ner]
+            classes = [0] * self.num_ner_classes()
+            classes[ner_cls[ner]] = 1
+            return classes
         except:
             print("Unexpected error:", sys.exc_info()[0])
             raise
-
-
+    
+    def num_ner_classes(self):
+        return len(ner_cls)
+    
+    def pad_seqlen(self, maxlen):
+        self.utterances = [self._pad_seqlen(utt, maxlen) for utt in self.utterances]
+    
+    def _pad_seqlen(self, seq, maxlen):
+        padzeros = [np.zeros(word2vec_dim, dtype=np.float32) for _ in xrange(maxlen - len(seq))]
+        return seq + padzeros
+        
 class AstroInput():
     
     def __init__(self, astro_data):
-        self.data = astro_data
+        self.utt = astro_data.utterances
+        self.ners = astro_data.ners
+        self.seqlen = astro_data.seqlen
+        self.num_ner_classes = astro_data.num_ner_classes()
+        self.batch_id = 0
+        self.maxlen = maxlen
+        self.dim = word2vec_dim
     
     def next(self, batch_size):
         """ Return a batch of data. When dataset end is reached, start over.
         """
-        if self.batch_id == len(self.data):
+        
+        if self.batch_id == len(self.utt):
             self.batch_id = 0
-        batch_data = (self.data[self.batch_id:min(self.batch_id +
-                                                  batch_size, len(self.data))])
-        batch_labels = (self.labels[self.batch_id:min(self.batch_id +
-                                                  batch_size, len(self.data))])
-        batch_seqlen = (self.seqlen[self.batch_id:min(self.batch_id +
-                                                  batch_size, len(self.data))])
-        self.batch_id = min(self.batch_id + batch_size, len(self.data))
-        return batch_data, batch_labels, batch_seqlen
-    
-def astro_producer(X, y, batch_size, max_steps, name=None):
-    """Iterate on the raw PTB data.
+        batch_data = self.utt[self.batch_id:min(self.batch_id +
+                                                  batch_size, len(self.utt))]
+        batch_labels = self.ners[self.batch_id:min(self.batch_id +
+                                                  batch_size, len(self.ners))]
+        batch_labels = [e for sublist in batch_labels for e in sublist]
+        batch_seqlen = self.seqlen[self.batch_id:min(self.batch_id +
+                                                  batch_size, len(self.seqlen))]
+        batch_mask = np.zeros((batch_size * self.maxlen))
+        for i, l in enumerate(batch_seqlen):
+            batch_mask[i * self.maxlen : i * self.maxlen + l] = 1
+        self.batch_id = min(self.batch_id + batch_size, len(self.utt))
+        return batch_data, batch_labels, batch_seqlen, batch_mask
 
-    This chunks up raw_data into batches of examples and returns Tensors that
-    are drawn from these batches.
 
-    Args:
-        raw_data: one of the raw data outputs from ptb_raw_data.
-        batch_size: int, the batch size.
-        num_steps: int, the number of unrolls.
-        name: the name of this operation (optional).
-
-    Returns:
-        X and y, each shaped [batch_size, max_steps] with padding zero. 
-        seqlen: shaped [batch_size], sequence lengths
-
-    Raises:
-        tf.errors.InvalidArgumentError: if batch_size or num_steps are too high.
-    """
-    
-    with tf.name_scope(name, "AstroProducer", [X, y, batch_size, max_steps]):
-        X = tf.convert_to_tensor(X, name="raw_input", dtype=tf.float32)
-        y = tf.convert_to_tensor(y, name="raw_label", dtype=tf.int32)
-
-        data_len = tf.size(X)
-        batch_len = data_len // batch_size
-        data = tf.reshape(X[0 : batch_size * batch_len],
-                                            [batch_size, batch_len])
-
-        epoch_size = (batch_len - 1) // max_steps
-        assertion = tf.assert_positive(
-                epoch_size,
-                message="epoch_size == 0, decrease batch_size or num_steps")
-        with tf.control_dependencies([assertion]):
-            epoch_size = tf.identity(epoch_size, name="epoch_size")
-
-        i = tf.train.range_input_producer(epoch_size, shuffle=False).dequeue()
-        x = tf.strided_slice(data, [0, i * max_steps],
-                                                 [batch_size, (i + 1) * max_steps])
-        x.set_shape([batch_size, max_steps])
-        y = tf.strided_slice(data, [0, i * max_steps + 1],
-                                                 [batch_size, (i + 1) * max_steps + 1])
-        y.set_shape([batch_size, max_steps])
-        return X, y
+def load():
+    train_data, valid_data, test_data = load_from_path('./')
+    trainset = AstroInput(train_data)
+    validset = AstroInput(valid_data)
+    testset = AstroInput(test_data)
+    return trainset, validset, testset
